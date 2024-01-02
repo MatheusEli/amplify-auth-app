755dcfaefe951aca2ecce4c13863c0f77a5a20f5
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2024-01-02 11:09:11 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
+
+
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(config);
+
+
+const App = ({ signOut, user }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style='container'>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>Amplify Todos</h2>
     </div>
   );
-}
+};
 
-export default App;
+export default withAuthenticator(App);
